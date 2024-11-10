eca789ed0e78615c3d5332d139bbb63ae7e9de1d
GTNH-Colen <54497873+GTNH-Colen@users.noreply.github.com>
2024-11-10 02:00:41 +0000

    Load command

--- src/main/java/com/myname/mymodid/commands/CommandLoadTest.java
+++ src/main/java/com/myname/mymodid/commands/CommandLoadTest.java
@@ -2,12 +2,21 @@ package com.myname.mymodid.commands;
 
 import com.myname.mymodid.MyMod;
 import com.myname.mymodid.Test;
+import com.myname.mymodid.procedures.AddItems;
+import com.myname.mymodid.procedures.Procedure;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.ChunkCoordinates;
 
+import java.util.ArrayList;
+
 public class CommandLoadTest extends CommandBase {
+
+    Test loadedTest;
+
     @Override
     public String getCommandName() {
         return "loadtest";
@@ -29,6 +38,13 @@ public class CommandLoadTest extends CommandBase {
 
         if (test.areChunksLoadedForStructure()) {
             test.buildStructure();
+            AddItems addItems = new AddItems();
+            addItems.x = 0;
+            addItems.y = 0;
+            addItems.z = 2;
+            addItems.itemsToAdd = new ArrayList<>();
+            addItems.itemsToAdd.add(new ItemStack(Items.diamond, 15));
+            addItems.handleEvent(test);
         } else {
             sender.addChatMessage(new ChatComponentText("This test exceeds the loaded area, it cannot be instantiated. It is likely too big."));
         }
