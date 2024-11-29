39f880e285bf7748a11c832c2b8ad6ffc5d2d31c
GTNH-Colen <54497873+GTNH-Colen@users.noreply.github.com>
2024-11-29 22:23:13 +0000

    Java fx work

--- src/main/java/com/gtnewhorizons/CTF/ClientProxy.java
+++ src/main/java/com/gtnewhorizons/CTF/ClientProxy.java
@@ -1,6 +1,26 @@
 package com.gtnewhorizons.CTF;
 
+import com.gtnewhorizons.CTF.ui.MainApp;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import javafx.application.Application;
+import javafx.application.Platform;
+
+import static javafx.application.Application.launch;
+
 public class ClientProxy extends CommonProxy {
+    @Override
+    public void preInit(FMLPreInitializationEvent event) {
+        super.preInit(event);
+
+        Thread fxThread = new Thread(() -> {
+            // Launch JavaFX application directly within the thread
+            Application.launch(MainApp.class);
+        });
+
+        // Set the thread as daemon so it closes when the application stops
+        fxThread.setDaemon(true);
+        fxThread.start();
+    }
 
     // Override CommonProxy methods here, if you want a different behaviour on the client (e.g. registering renders).
     // Don't forget to call the super methods as well.
---
+++ src/main/java/com/gtnewhorizons/CTF/ui/MainApp.java
@@ -0,0 +1,12 @@
+package com.gtnewhorizons.CTF.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Hello JavaFX!");
+        stage.show();
+    }
+}
